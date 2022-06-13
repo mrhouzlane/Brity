86ada3610aa7986e15f9eb87fbe2a0e98215095b
mrhouzlane <mrhouzlane@github.com>
2022-06-13 12:44:50 +0200

    FirstPageDone

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,17 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import HB from "../images/backG.png"
 //import logo from "../images/Logo.png";
-import {ConnectButton, Select} from "web3uikit";
+import {ConnectButton, Select, DatePicker, Input, Icon} from "web3uikit";
+import { useState } from "react";
 
 
 
 const Home = () => {
+  const [MetaCheckIN, setMetaCheckIN] = useState(new Date());
+  const [MetaCheckOut, setMetaCheckOut] = useState(new Date());
+  const [destination, setDestination] = useState("Miami");
+  const [guests, setGuest] = useState(2);
+
 
   return (
     <>
@@ -30,19 +36,70 @@ const Home = () => {
         <div className="searchFields">
         <div className="inputs">
           MetaLocation
+          <Select
+              defaultOptionIndex={0}
+              onChange={(data) => setDestination(data.labetl)}
+              options={[
+                {
+                  id: 'Mi',
+                  label: 'Miami',
+                },
+                {
+                  id: 'CS',
+                  label: 'Casablanca',
+                },
+                {
+                  id: 'la',
+                  label: 'Los Angeles',
+                },
+                {
+                  id: 'RB',
+                  label: 'Rabat',
+                },
+                {
+                  id: 'TG',
+                  label: 'Tanger',
+                },
+
+              ]}
+            />
         </div>
         <div className="vl"/>
         <div className="inputs">
           MetaCheck In 
+          <DatePicker
+              id="MetaCheckIN"
+              onChange={(event) => setMetaCheckIN(event.data)}
+            />
         </div>
         <div className="vl"/>
         <div className="inputs">
           MetaCheck Out
+          <DatePicker
+              id="MetaCheckOut"
+              onChange={(event) => setMetaCheckOut(event.data)}
+            />
         </div>
         <div className="vl"/>
         <div className="inputs">
           Guests
+          <Input
+              label="guests"
+              type="number"
+              onBlur={function noRefCheck(){}}
+              onChange={(event)=> setGuest(Number(event.target.value))}
+            />
         </div>
+        <Link to={"/rentals"} state={{
+          destination: destination,
+          MetaCheckIN: MetaCheckIN,
+          MetaCheckOut: MetaCheckOut,
+          guests: guests
+        }}>
+        <div className="searchButton">
+          <Icon fill="#FFFFFF" size={30} svg="search"></Icon>
+        </div>
+        </Link>
         </div> 
       </div>
     </>
--- src/pages/Rentals.js
+++ src/pages/Rentals.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Rentals.css";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
+
+
 
 const Rentals = () => {
 
+  const {}
   return (
     <>
      <Link to="/"> Home </Link>
