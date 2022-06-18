47f7ed70b737b446b56647654f708f26e3459a0a
mrhouzlane <mrhouzlane@github.com>
2022-06-18 17:45:16 +0200

    search Filtering

--- src/pages/Rentals.js
+++ src/pages/Rentals.js
@@ -2,15 +2,37 @@ import React from "react";
 import "./Rentals.css";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
+import logo from "../images/Logo.png";
+import {ConnectButton} from "web3uikit";
 
 
 
 const Rentals = () => {
 
-  const {state: searchFilters} = useLocation()
+  const {state: searchFilters} = useLocation();
   return (
     <>
-     <Link to="/"> Home </Link>
+      <div className="topBanner">
+        <div>
+          <Link to="/">
+            <img className="logo" src={logo} alt="logo"></img>
+          </Link>
+        </div>
+        <div className="searchReminder"></div>
+
+        <div className="filter">
+        {searchFilters.destination}
+        </div>
+        <div className="vl" />
+        <div className="filter"></div>
+        <div className="vl" />
+        <div className="filter"></div>
+        <div className="vl" />
+        
+        <div className="lrContainers">
+          <ConnectButton />
+        </div>
+      </div>
     </>
   );
 };
